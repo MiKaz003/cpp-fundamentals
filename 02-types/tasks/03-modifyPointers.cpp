533514b606cf1ac9740b1c335330ea73a675b406
Michał Kaźmierczak <ghmkazmierczak@gmail.com>
2024-07-09 20:49:21 +0200

    modifyPtr

--- 02-types/tasks/03-modifyPointers.cpp
+++ 02-types/tasks/03-modifyPointers.cpp
@@ -2,6 +2,17 @@
 
 // TODO: Implement foo() and bar()
 // foo() should modify value under passed pointer to 10
+int foo(int *number){
+    *number = 10;
+
+    return *number;
+}
+
+int bar(int *number){
+    *number = 20;
+
+    return *number;
+}
 // bar() should modify value under passed pointer to 20
 // Can we have a pointer to const or a const pointer?
 
