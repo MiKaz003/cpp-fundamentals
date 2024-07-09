6eaeca9d4a0459bf2be2f54951b9c35ce212a90b
MiKaz <ghmkazmierczak@gmail.com>
2024-07-09 21:52:57 +0200

    02 done

--- 02-types/tasks/02-modifyString.cpp
+++ 02-types/tasks/02-modifyString.cpp
@@ -3,7 +3,7 @@
 
 // TODO: Implement modifyString()
 std::string modifyString(std::string& str){
-    str = "Other string";
+    str = "Other string ";
     return str;
 }
 // It should modify passed string to text "Other string"
